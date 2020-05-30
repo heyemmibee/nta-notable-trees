45f333d38b0c132c5252107b9717f0d52d2f21bc
heyemmibee <eburnaman@gmail.com>
2020-05-30 11:58:40 -0400

    map is working

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,27 @@
-let map; 
-document.addEventListener('DOMContentLoaded', () => {
-    let s = document.createElement('script');
-    document.head.appendChild(s);
-    s.addEventListener('load', () => {
-        console.log('did you see me load?');
-        s.src = `https://maps.googleapis.com/maps/apis/js?key=${APIKEY}`;
-    });
-})
+// let map; 
+// document.addEventListener('DOMContentLoaded', () => {
+//     let s = document.createElement('script');
+//     document.head.appendChild(s);
+//     s.addEventListener('load', () => {
+//         console.log('did you see me load?');
+//         map = new google.maps.Map(document.getElementById('tree-map'), {
+//         center: {
+//             lat:115, 
+//             lng:-213
+//         },
+//         zoom: 15,
+//         mapTypeId: google.maps.mapTypeId.ROADMAP
+//     });
+//         s.src = `https://maps.googleapis.com/maps/apis/js?key=${APIKEY}&callback=initMap`;
+//     });
+// })
+
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('tree-map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }; 
